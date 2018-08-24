56312229dfa3f35316559bacb45ed8e67b040f7f
John Manoah <johnmanoah@MacBook-Pro.local>
2018-08-23 19:39:11 -0700

    confirm booking

--- src/app/confirmbooking/confirmbooking.component.ts
+++ src/app/confirmbooking/confirmbooking.component.ts
@@ -1,21 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { TripService } from './../trip.service';
 import { Router } from '@angular/router';
 import { TripcardComponent } from './../tripcard/tripcard.component';
 import { UserService } from './../user.service';
 import 'rxjs/add/operator/take';
+declare let paypal: any;
 
 @Component({
   selector: 'app-confirmbooking',
   templateUrl: './confirmbooking.component.html',
   styleUrls: ['./confirmbooking.component.css']
 })
-export class ConfirmbookingComponent implements OnInit {
+export class ConfirmbookingComponent implements OnInit, AfterViewChecked  {
 	trip;
 	trip$;
 	adulttotal;
 	childtotal;
 	grandtotal: number;
+    addScript: boolean = false;
+  paypalLoad: boolean = true;
+  
+  finalAmount: number=0.01;
+ 
+  paypalConfig = {
+    env: 'sandbox',
+    client: {
+      sandbox: 'AS-q2vpC-wfMbHEgCiUT6mgqFIF9nyjOeF2kKRIV1uDuepmAUrndQhqRcD2dMUM-qOfva34jS0gTSYc5',
+      production: '<your-production-key here>'
+    },
+    commit: true,
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            { amount: { total: this.finalAmount, currency: 'CAD' } }
+          ]
+        }
+      });
+    },
+    onAuthorize: (data, actions) => {
+      return actions.payment.execute().then((payment) => {
+        this.book('PayPal');
+      })
+    }
+  };
+
 
   constructor(
                 private tripService: TripService, 
@@ -51,9 +80,29 @@ export class ConfirmbookingComponent implements OnInit {
 
   }
 
-  book(form){
+
+ ngAfterViewChecked(): void {
+    if (!this.addScript) {
+      this.addPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
+        this.paypalLoad = false;
+      })
+    }
+  }
+  
+  addPaypalScript() {
+    this.addScript = true;
+    return new Promise((resolve, reject) => {
+      let scripttagElement = document.createElement('script');    
+      scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scripttagElement.onload = resolve;
+      document.body.appendChild(scripttagElement);
+    })
+  }
+
+  book(paymentmode){
     //console.log(form.paymentmode)
-  	this.trip.paymentmode = form.paymentmode;
+  	this.trip.paymentmode = paymentmode;
     this.trip.datebooked = Date.now();
     this.trip.status = 'Pending confirmation';
     console.log('in book', this.trip);
